5fa304bc03d31152feae97104b4dddb86bc36100
VachhaniVaishvi96 <vaishvivachhani@gmail.com>
2025-12-23 11:00:37 +0530

    first commit

---
+++ npm-lab-9/index.js
@@ -0,0 +1,34 @@
+// const express=require('express')
+// const mongoose=require('mongoose')
+// require('dotenv').config()
+// const app=express();
+// const user=require('./module/user')
+// app.use(express.json())
+
+// mongoose.connect(process.env.mongourl),then(()=>{
+//     console.log("mongo connected")
+// }).catch((err)=>{
+//     console.log(err)
+// })
+
+// app.post('/register',async(req,res)=>{
+//     const User=new user({
+//         "name":"xyz",
+//         "email":"xyz@xyz",
+//         "password":"123654",
+//         "phone":"1236547890"
+//     });
+
+//     const newUser=awaitUser.save()
+//     return res.status(200).json({message:"success",newUser})
+// })
+
+// app.listen(3000,()=>{
+//     console.log("done")
+// })
+
+
+
+//CRUD
+
+
